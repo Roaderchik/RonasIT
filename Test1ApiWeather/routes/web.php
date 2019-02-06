321e48e33e3298527073abf307bc8387ec3354fe
root <root@179151.simplecloud.ru>
2019-02-06 11:41:34 +0300

    Edit Router getListCountry

--- Test1ApiWeather/routes/web.php
+++ Test1ApiWeather/routes/web.php
@@ -16,5 +16,24 @@ Route::get('/', function () {
 });
 
 Route::get('getListCountry/{CountrySearch?}', function ($CountrySearch = Null) {
-    return 'CountrySearch '.$CountrySearch;
+    //return 'CountrySearch '.$CountrySearch;
+	
+	//Если не использовать пакеты типа LaraCurl, Guzzle и т.п. 
+	
+	
+	if (is_null($CountrySearch )) {
+		//Если нет уточняющих данных передаем все старны
+		$json = json_decode(file_get_contents('https://restcountries.eu/rest/v2/all'), true);
+	} else {
+		//Передаюм только содержащие буквы 
+		$json = json_decode(file_get_contents('https://restcountries.eu/rest/v2/name/'.$CountrySearch), true);
+	}
+	
+	//Очищаем от лишних данных
+	$response=[];
+	foreach($json as $key => $search)  {      				
+		$response[]=array('country_code' => $search['alpha2Code'], 'name' => $search['name'], 'name_local'=> $search['nativeName']);         
+    }  	
+	
+	return Response::json($response);
 });
