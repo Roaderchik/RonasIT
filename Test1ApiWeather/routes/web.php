60f5fd247fed7c4f43c338ff63ae7b4b7feb6cef
Юрий Цигвинцев <web21veka@gmail.com>
2019-06-10 17:57:53 +0300

    remove trash routing

--- Test1ApiWeather/routes/web.php
+++ Test1ApiWeather/routes/web.php
@@ -15,37 +15,6 @@ Route::get('countries', 'LocateContoroller@getListCountries')->name('locate.coun
 Route::get('countries/{countryCode}/cities', 'LocateContoroller@getListCities')->name('locate.cities')->where(['countryCode' => '[A-Z]+']);;
 
 
-
-
-Route::get('getListCity/{CountryCode}/{CitySearch?}', function ($CountryCode,$CitySearch = Null) 
-{  
-//вот это вообще конечно жесть, в идеале передеалать на отдельный микросервис и получать оттуда уже фильтрованные данные
-
-$path = storage_path() . "/files/city.list.json"; 
-$json = json_decode(file_get_contents($path), true); 
-
-
-
-	$response=[];
-	foreach($json as $key => $search)  {    
-	
-		if ($search['country']== strtoupper($CountryCode)) {
-			if (is_null($CitySearch )) {
-				$response[]=$search; 
-			} else {
-				if ( stripos($search['name'], $CitySearch)   !== false) {
-					$response[]=$search;
-				}
-			}
-		}		
-    }
-	 
-	return Response::json($response);
-});
-
-
-
-
 Route::get('weather/city/{cityId}/', 'WeatherContoroller@getCurrentWeatherCity')->name('weather.city')->where(['cityId' => '[0-9]+']);
 
 Route::get('weather/geo/{lat}/{lon}/', 'WeatherContoroller@getCurrentWeatherGeoCoor')->name('weather.geocoor')->where(['lat' => '[0-9,.]+','lon' => '[0-9,.]+']);
